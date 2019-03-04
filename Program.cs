0a862a108220162cc34e2740ab461aebae1dfadb
THATBLADEBOY <aus10b@gmail.com>
2019-03-04 17:03:31 -0600

    initial commit; practice complete

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace iterating_planets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>(){"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
+            List<Dictionary<string, string>> probes = new List<Dictionary<string, string>>();
+            probes.Add (new Dictionary<string, string>(){ {"Mercury", "Whatever"} });
+            probes.Add (new Dictionary<string, string>(){ {"Venus", "Akatsuki"} });
+            probes.Add (new Dictionary<string, string>(){ {"Earth", "We Live Here"} });
+            probes.Add (new Dictionary<string, string>(){ {"Mars", "Odyssey"} });
+            probes.Add (new Dictionary<string, string>(){ {"Mars", "Express"} });
+            probes.Add (new Dictionary<string, string>(){ {"Mars", "Reconnaissance Orbiter"} });
+            probes.Add (new Dictionary<string, string>(){ {"Jupiter", "Juno"} });
+            probes.Add (new Dictionary<string, string>(){ {"Saturn", "Voyager 1"} });
+            probes.Add (new Dictionary<string, string>(){ {"Uranus", "Voyager 2"} });
+            probes.Add (new Dictionary<string, string>(){ {"Neptune", "Mickey Mouse"} });
+
+            foreach(string planet in planetList) {
+                List<string> matchingProbes = new List<string>();
+                foreach(Dictionary<string, string> probe in probes){
+                   if(probe.ContainsKey(planet)){
+                       foreach(KeyValuePair<string, string> thing in probe){
+                           matchingProbes.Add(thing.Value);
+                       }
+                   }
+                }      
+                Console.WriteLine($"{planet}: {String.Join(",", matchingProbes)}"); 
+            }
+        }
+    }
+}
